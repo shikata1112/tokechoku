82e7324251007bd8edf13780f168d9b646e70515
era <hera123230@gmail.com>
2021-03-21 16:55:52 +0900

    vendorモデルを作成

---
+++ db/migrate/20210320112648_devise_create_vendors.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class DeviseCreateVendors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :vendors do |t|
+      ## Database authenticatable
+
+      t.string :last_name, null: false, comment: "姓"
+      t.string :first_name, null: false, comment: "名"
+      t.string :kana_last_name, null: false, comment: "セイ"
+      t.string :kana_first_name, null: false, comment: "メイ"
+      t.string :shop_name, null: false, comment: "店舗名"
+      t.text   :introduction, comment: "店舗紹介"
+      t.string :email, null: false, comment: "メールアドレス"
+      t.string :birth_date, null: false, comment: "生年月日"
+      t.string :phone_number, null: false, comment: "電話番号"
+      t.string :postcode, null: false, comment: "郵便番号"
+      t.string :prefacture, null: false, comment: "都道府県"
+      t.string :city, null: false, comment: "市区町村"
+      t.string :address, null: false, comment: "番地"
+      t.string :building, comment: "ビル・建物名"
+      t.string :encrypted_password, null: false, comment: "パスワード"
+      t.boolean :is_active, null: false, default: true, comment: "有効性"
+      t.boolean :is_certificated, null: false, default: false, comment: "身分証明済み"
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      t.timestamps null: false
+    end
+
+    add_index :vendors, :email,                unique: true
+    add_index :vendors, :reset_password_token, unique: true
+  end
+end
---
+++ spec/factories/vendors.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :vendor do
+    
+  end
+end
