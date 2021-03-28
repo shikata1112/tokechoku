db19e60bab26bf5b3905055a35a29480ffd85faf
shushikata <shu.42111227@gmail.com>
2021-03-28 19:26:21 +0900

    userのルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,14 @@ Rails.application.routes.draw do
   
   scope module: :user do
     resources :orders, only: [:new, :create, :show], path_names: { new: "process" } do
-      get "history", to: "orders#index", on: :collection
-      get "payment", to: "orders#payment", on: :collection
+      collection do
+        get "history", to: "orders#index"
+        get "payment", to: "orders#payment"
+        get "completed", to: "orders#thanks"
+      end
     end
 
-    resource :user, only: [:show, :edit, :update]
+    resource :user, only: [:edit, :update]
     resources :addresses, only: [:index, :create, :edit, :update, :destroy]
     resources :watch_lists, only: [:index, :create, :destroy]
     resources :cards, only: [:index, :create, :edit, :update, :destroy]
