89177c9257712ecbcc9b8740cf01bae06466cb0d
shushikata <shu.42111227@gmail.com>
2021-03-29 14:42:01 +0900

    adminのルーティグとコントローラの作成

---
+++ app/controllers/admin/members_controller.rb
@@ -0,0 +1,13 @@
+class Admin::MembersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
---
+++ app/controllers/admin/vendors_controller.rb
@@ -0,0 +1,13 @@
+class Admin::VendorsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
